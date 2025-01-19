483234762055e2ef60c0c5372a7097e409159cee
Karthik kotekar <152687159+karthiks779@users.noreply.github.com>
2025-01-19 19:04:50 +0530

    Add files via upload

---
+++ H.js
@@ -0,0 +1,6 @@
+const sugg = {
+            list1: ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'],
+            list2: ['Car', 'Bus', 'Bike', 'Truck', 'Plane'],
+            list3: ['Dog', 'Cat', 'Rabbit', 'Bird', 'Fish'],
+            list4: ['Red', 'Blue', 'Green', 'Yellow', 'Purple']
+        };
